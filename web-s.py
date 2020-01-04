dadf4f6cf0755b81e9daabf159fa7a61e9ff20a3
jerry8tom <mukul02015@gmail.com>
2020-01-05 01:57:13 +1100

    les use ~soup~ soop.

--- web-s.py
+++ web-s.py
@@ -15,11 +15,14 @@ driver.get("https://www.flipkart.com/search?q=laptop")
 content = driver.page_source
 soup = BeautifulSoup (content, "lxml")
 count = 1
-for a in soup.findAll ('a', href = True, attrs = {'class':'_31qSDS'}) and count < 50:
-	name = a.find ('div', attrs = {'class':'_3sU53n'})
-	price = a.find ('div', attrs = {'class':'_1vC40E _2rQ-NK'})
-	rating = a.find ('div', attrs = {'class':'hGSR34 _2beYZw'})
+for a in soup.findAll ('a', href = True, attrs = {'class':'_31qSD5'}) and count < 50:
+	name = a.find ('div', attrs = {'class':'_3wU53n'})
+	price = a.find ('div', attrs = {'class':'_1vC4OE _2rQ-NK'})
+	rating = a.find ('div', attrs = {'class':'hGSR34'})
 	products.append(name.text)
 	prices.append(price.text)
 	ratings.append(rating.text)
 	count = count + 1
+
+df = pd.DataFrame({'Product Name':products,'Price':prices,'Rating':ratings}) 
+df.to_csv('products.csv', index=False, encoding='utf-8')
